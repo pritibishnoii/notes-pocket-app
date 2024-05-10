805ad490fac67aaa252fd588b60cf77ae8c0214e
priti bishnoi <priti12bishnoi@gmail.com>
2024-05-10 15:05:01 +0530

    updated

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -18,7 +18,7 @@ function NoteContainer() {
 
   const addTextNotes = (e) => {
     if (text.trim() !== '') {
-     
+
     }
   }
   return (
@@ -44,37 +44,43 @@ function NoteContainer() {
       </div> */}
 
 
-<div className="chat-container" style={{backgroundColor:'#dae5f5',position:'absolute',left:'29%',width:'71%'}}>
-<div className="chatSc">
-        <div className="header" style={{width:'100%',height:'90px',backgroundColor:'blue',display:'flex',gap:'22px'}}>
-          <img src={backIcon} alt="back"  style={{width:'22px',height:'22px',marginTop:'22px',marginLeft:'22px'}}/>
-          <h1 >HM</h1>
-          <h1>html ml</h1>
-        </div>
+      <div className="chat-container">
+        <div className="chatSc">
+          <div className="header">
+            {/* <img src={backIcon} alt="back" className='back-img' /> */}
+            <h1 >HM</h1>
+            <h1>html ml</h1>
+          </div>
 
-        <div className="chat-input" style={{ marginTop: '388px',width:'100%',height:'25vh',backgroundColor:'blue' }}>
-          <textarea
-          style={{resize:'none',width:'93%',height:'18vh',backgroundColor:'#fff',margin:'22px'}}
-            value={text}
-            onChange={setTextArea}
-            placeholder="Enter your text here"
-          ></textarea>
-          <div
-            onClick={addTextNotes}
-            className="send-btn"
-            style={{  height: '70px',width:'70px',position:'absolute' ,bottom:'32px' ,right:'8%'}}>
-            <img src={sendIcon} alt="send" style={{ margin: '32px '}} />
+          <div className="note-box">
+            <p className='text-note'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia sunt veritatis sequi dolorum delectus mollitia sit? Similique qui quasi, inventore, tempore repellendus esse fugiat ex officia omnis, voluptatum nulla earum.</p>
+            <p className='date'>9 Mar 2023</p>
+            <p className='time'>10:00 AM</p>
+          </div>
+
+          <div className="chat-input">
+            <textarea
+
+              placeholder="Enter your text here"
+              value={text}
+              onChange={(e)=>setText(e.target.value)}
+            ></textarea>
+            <div
+              onClick={addTextNotes}
+              className="send-btn"
+            >
+              <img src={sendIcon} alt="send" />
+            </div>
           </div>
         </div>
       </div>
-</div>
 
 
 
 
 
 
-      
+
 
 
     </>
